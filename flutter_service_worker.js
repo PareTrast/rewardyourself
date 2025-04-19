5cc494329276b3b5f5c8e642b230394b5695e055
Kaelin <43419622+PareTrast@users.noreply.github.com>
2025-04-18 23:20:59 -0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/app/app.zip": "87dc78dbc422c633b05ee3ef85c8eadd",
-"assets/app/app.zip.hash": "cf04c6caa0f5626463a5a49b70349a01",
+const RESOURCES = {"assets/app/app.zip": "bef0e282c7304b60c324c5f551f6146b",
+"assets/app/app.zip.hash": "c585b4db3366b0f19a38c87aa42dd571",
 "assets/AssetManifest.bin": "5e4c69c57b629fa3c0fbf78c71c4db94",
 "assets/AssetManifest.bin.json": "992ba04d5892193176350dad1f95a2ec",
 "assets/AssetManifest.json": "5515896244b15ec7f3fc5ce45479796d",
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/app/app.zip": "87dc78dbc422c633b05ee3ef85c8eadd",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "dd018003369bee3e244a8cc59f119e5f",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "3c898a468530fb635dbbb0058178bee3",
+"flutter_bootstrap.js": "578e87cc1849406a70dd0296927b9e80",
 "icons/Icon-192.png": "817caa1985cf73eaaa8dd02dda218970",
 "icons/Icon-512.png": "1b5e2bffe69da59cfd351c35c744bcdc",
 "icons/Icon-maskable-192.png": "817caa1985cf73eaaa8dd02dda218970",
